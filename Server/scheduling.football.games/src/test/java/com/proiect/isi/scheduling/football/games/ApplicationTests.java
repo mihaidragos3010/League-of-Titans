bfe19ad0d716f7f8eaab9dd2d7b920eadeef2876
MIHAI Dragos-Andrei <dragos.mihai3010@stud.acs.upb.ro>
2024-12-04 13:07:35 +0200

    Init server and database

---
+++ Server/scheduling.football.games/src/test/java/com/proiect/isi/scheduling/football/games/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.isi.scheduling.football.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
