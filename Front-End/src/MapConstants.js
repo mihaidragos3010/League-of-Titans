77bcecb3efdd58f18fa8c45c513f524ce9e89233
Vlad Ștefan SÎRBU <vlad_stefan.sirbu@stud.acs.upb.ro>
2025-01-14 01:59:12 +0200

    Added Geolocation, MapView buttons and points, fixed requests

---
+++ Front-End/src/MapConstants.js
@@ -0,0 +1,7 @@
+// Playground ocupation status colors
+export const OCCUPATION_STATUS_COLORS = {
+    AVAILABLE: "lightgreen",
+    WAITING: "yellow",
+    OCCUPIED: "red",
+    UNKNOWN: "gray",
+};
